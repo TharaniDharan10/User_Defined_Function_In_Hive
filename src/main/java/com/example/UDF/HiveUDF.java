9c8f82d9b84bc3e28c34cb2b2e32fdd7dd246b1d
TharaniDharan10 <dharunjisnu109@gmail.com>
2025-11-03 01:11:35 +0530

    made 2 udf functions

---
+++ src/main/java/com/example/UDF/HiveUDF.java
@@ -0,0 +1,31 @@
+package com.example.UDF;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+public class HiveUDF extends UDF {
+
+	private Text result = new Text();
+	// Example: Convert string to uppercase
+	public Text evaluate(Text Name) {
+		if (Name == null) {
+			return null;
+		}
+		String res = Name.toString().toUpperCase();
+		result = new Text(res);
+		return result;
+	}
+
+	public Text evaluate(IntWritable id, Text Name) {
+		if (id == null) {
+			return null;
+		}
+		if (Name == null) {
+			return null;
+		}
+		String res = Integer.toString(id.get()) + "-" + Name.toString();
+		result = new Text(res);
+		return result;
+	}
+}
